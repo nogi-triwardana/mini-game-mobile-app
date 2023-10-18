43964496fff4749f7660c93c41b887e51d026888
nogi.triwardana <nogi.triwardana@smt-id.com>
2023-10-18 18:59:40 +0700

    initial create project

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
+import { useState } from 'react';
+import { View, StyleSheet, ImageBackground, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Colors from './constants/colors';
+
+import StartGameScreen from './screens/startGameScreen';
+import GameScreen from './screens/gameScreen';
+import GameOverScreen from './screens/gameOverScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState('');
+  const [gameIsOver, setIsGameOver] = useState(true);
+  const [guessRounds, setGuessRounds] = useState(0); 
+
+  const [fontLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  })
+
+  if(!fontLoaded) {
+    return <AppLoading />
+  }
+
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+    setIsGameOver(false);
+  }
+
+  function gameOverHandler(numberOfRounds) {
+    setIsGameOver(true);
+    setGuessRounds(numberOfRounds);
+  }
+  
+  function startNewGameHandler() {
+    setUserNumber(null);
+    setGuessRounds(0);
+  }
+
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
+
+  if(userNumber) {
+    if(gameIsOver) {
+      screen = (
+        <GameOverScreen 
+          roundsNumber={guessRounds} 
+          userNumber={userNumber} 
+          onStartNewGame={startNewGameHandler} 
+        />
+      );
+    } else {
+      screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
+    }
+
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <LinearGradient 
+        colors={[Colors.primary700, Colors.accent500]} 
+        style={styles.rootScreen}
+      > 
+        <ImageBackground 
+          source={require('./assets/images/background.png')}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  backgroundImage: {
+    opacity: 0.15
+  }
 });
---
+++ components/ui/Title.js
@@ -0,0 +1,27 @@
+import { Text, StyleSheet, Platform } from "react-native";
+
+import Colors from "../../constants/colors";
+
+function Title({children}) {
+  return (
+    <Text style={styles.title}>{children}</Text>
+  );
+}
+
+export default Title;
+
+const styles = StyleSheet.create({
+  title: {
+    fontFamily: 'open-sans-bold',
+    fontSize: 24,
+    // fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    // borderWidth: Platform.OS === 'android' ? 2 : 0,
+    borderWidth: Platform.select({ios: 2, android: 0 }),
+    borderColor: 'white',
+    padding: 12,
+    maxWidth: '80%',
+    width: 300
+  }
+});
